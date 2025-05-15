6ff71429eb9e2e567159d58afb28b4ec81d0eb02
purplemoonintelligence <purplemoonintelligence@gmail.com>
2025-05-16 08:57:43 +1000

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/site/wwwroot
+cd /home/site/wwwroot/purplelyrics
 pip install -r requirements.txt
-uvicorn main:app --host 0.0.0.0 --port 8000
+exec uvicorn main:app --host 0.0.0.0 --port $PORT
